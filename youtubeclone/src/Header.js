fe7afae92a715c40ed22ecb097b27ea10810ba1f
SYEDHASNAT1997 <79423718+SYEDHASNAT1997@users.noreply.github.com>
2021-05-28 18:16:35 +0500

    Create Header.js

---
+++ youtubeclone/src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuIcon, { MenuItem } from "@material-ui/core"
+
+function Header() {
+    return (
+        <div className="header">
+
+        <MenuItem></MenuItem>
+        </div>
+    )
+}
+
+export default Header
